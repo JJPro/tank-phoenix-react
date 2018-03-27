a2a33dadbc798248b64aa7078ae8b534cd1e69f7
LU JI <jjpro@LUs-MacBook-Pro-3.local>
2018-03-27 06:15:10 -0400

    temp

--- tanks/assets/js/parts/tank.js
+++ tanks/assets/js/parts/tank.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Konva from 'konva';
-import { Stage, Layer, Rect, Text } from 'react-konva';
+import { Image } from 'react-konva';
 
-export default ({tank, unit}) => {
-  console.log(tank, unit);
-  return (<Rect x={tank.x*unit} y={tank.y*unit} width={tank.width*unit} height={tank.width*unit} stroke={"blue"} />);
+export default class Tank extends Component{
+  constructor(props){
+    super(props);
+    this.state = {image: null}
+  }
+
+  componentDidMount(){
+    console.log({thumbnail: this.props.tank.player.tank_thumbnail});
+    let image = new window.Image();
+    image.src = this.props.tank.player.tank_thumbnail;
+    image.onload = () => {
+      this.setState({image: image})
+    }
+  }
+  render(){
+    let x = this.props.tank.x,
+        y = this.props.tank.y,
+        width = this.props.tank.width,
+        height = this.props.tank.height,
+        unit = this.props.unit;
+    return (<Image image={this.state.image} width={width*unit} height={height*unit} />);
+  }
+// ({tank, unit}) => {
+//   console.log(tank, unit);
+//   // console.log(`"${tank.player.tank_thumbnail}"`);
+//   console.log(Image);
+//
+//   let imageObj = new window.Image();
+//   imageObj.src = "/images/tank-cyan.png";
+//   return (<Image image="/images/tank-cyan.png" x={tank.x*unit} y={tank.y*unit} width={tank.width*unit} height={tank.height*unit} />);
 }
