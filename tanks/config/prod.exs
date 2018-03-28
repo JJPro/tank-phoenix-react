8f07b3b81fd08ecfcca1eb6c82e5e6d88faa8e73
LU JI <jjpro@LUs-MacBook-Pro-3.local>
2018-03-27 22:55:49 -0400

    update config file

--- tanks/config/prod.exs
+++ tanks/config/prod.exs
@@ -56,7 +56,7 @@ config :logger, level: :info
 # Alternatively, you can configure exactly which server to
 # start per endpoint:
 #
-#     config :tanks, TanksWeb.Endpoint, server: true
+    config :tanks, TanksWeb.Endpoint, server: true
 #
 
 # Finally import the config/prod.secret.exs
