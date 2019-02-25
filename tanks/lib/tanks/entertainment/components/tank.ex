fa4cf70714d383fce3de85281fb3a9a74c4defa5
Jason Ji <lu.ji1@me.com>
2019-02-25 00:16:11 -0500

    refactor tank.ex

--- tanks/lib/tanks/entertainment/components/tank.ex
+++ tanks/lib/tanks/entertainment/components/tank.ex
@@ -1,4 +1,10 @@
 defmodule Tanks.Entertainment.Components.Tank do
-  alias Tanks.Accounts.User
+  alias __MODULE__
+  @enforce_keys [:orientation, :player]
+  # alias Tanks.Accounts.User
   defstruct x: 0, y: 0, width: 2, height: 2, hp: 4, orientation: nil, player: nil, image: ""
+
+  def new do
+    %Tank{orientation: :down, player: nil}
+  end
 end
